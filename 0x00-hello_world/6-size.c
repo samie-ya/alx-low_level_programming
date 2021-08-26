11761a8b9ff60a32db259244837273ed41d66fa0
Samie <samrasolomon40@gmail.com>
2021-08-26 09:50:59 -0700

    Modified a file

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,10 +10,10 @@ char c;
 float f;
 long int li;
 long long int lli;
-printf("Size of an int:%d byte(s)\n", sizeof(i));
-printf("Size of a char:%d byte(s)\n", sizeof(c));
-printf("Size of a float:%d byte(s)\n", sizeof(f));
-printf("Size of a long int:%d byte(s)\n", sizeof(li));
-printf("Size of a long long int:%d byte(s)\n", sizeof(lli));
+printf("Size of a char: %d byte(s)\n", sizeof(c));
+printf("Size of an int: %d byte(s)\n", sizeof(i));
+printf("Size of a long int: %d byte(s)\n", sizeof(li));
+printf("Size of a long long int: %d byte(s)\n", sizeof(lli));
+printf("Size of a float: %d byte(s)\n", sizeof(f));
 return (0);
 }
