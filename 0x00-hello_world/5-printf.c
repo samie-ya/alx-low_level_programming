d6edf565ce0755cda2b5fb4da676f3b644027f3c
Samie <samrasolomon40@gmail.com>
2021-08-26 09:46:24 -0700

    Modified a file

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
-main()
+/**
+ * main - will be printing a statement and returning zero value.
+ * Return: Always 0.
+ */
+int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,4 +1,8 @@
 #include <stdio.h>
+/**
+ * main - will be printing sizes of type and returning zero value.
+ * Return: Always 0.
+ */
 int main(void)
 {
 int i;
@@ -6,10 +10,10 @@ char c;
 float f;
 long int li;
 long long int lli;
-printf("Size of a int:%lt\n", (unsigned long)sizeof(i));
-printf("Size of a char:%lt\n", (unsigned long)sizeof(c));
-printf("Size of a float:%lt\n", (unsigned long)sizeof(f));
-printf("Size of a long int:%lt\n", (unsigned long)sizeof(li));
-printf("Size of a long long int:%lt\n", (unsigned long)sizeof(lli));
+printf("Size of an int:%d byte(s)\n", sizeof(i));
+printf("Size of a char:%d byte(s)\n", sizeof(c));
+printf("Size of a float:%d byte(s)\n", sizeof(f));
+printf("Size of a long int:%d byte(s)\n", sizeof(li));
+printf("Size of a long long int:%d byte(s)\n", sizeof(lli));
 return (0);
 }
