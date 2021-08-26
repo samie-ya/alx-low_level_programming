3d9b741f6482afbb24eb2d9e86b90332dafeac01
Samie <samrasolomon40@gmail.com>
2021-08-26 12:59:33 -0700

    Modifies a file

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
  * main - will be printing a statement and returning one value.
- * Return: Always 0.
+ * Return: Always 1.
  */
 int main(void)
 {
-fprintf(stderr, "and that piece of art is useful\"\- Dora Korpar, 2015-10-19");
+fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19");
 return (1);
 }
