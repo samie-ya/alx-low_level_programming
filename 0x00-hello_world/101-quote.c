87de0b177cf705ae96fded7cc56f4dc2b22cc314
Samie <samrasolomon40@gmail.com>
2021-08-26 11:01:50 -0700

    Added file

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - will be printing a statement and returning one value.
+ * Return: Always 0.
+ */
+int main(void)
+{
+putchar("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+return (1);
+}
