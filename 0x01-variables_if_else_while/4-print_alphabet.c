fe12ed95f1f3e8946076bfd2fa93161f314ba78c
Samie <samrasolomon40@gmail.com>
2021-08-27 09:22:03 -0700

    Added a file

---
+++ 0x01-variables_if_else_while/4-print_alphabet.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/**
+ * main - will not output q an e
+ *
+ * Return: value 0
+ */
+int main(void)
+char a = a;
+if (i = 'a')
---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - will be printing numbers in decending order. 
+ *
+ * Return: value 0
+ */
+int main(void)
+{
+int i;
+for (i = 0; i < 10; i++)
+{
+putchar((i % 10) + '0') ,;
+}
+return (0);
+}
