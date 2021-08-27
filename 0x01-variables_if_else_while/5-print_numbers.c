a9015df5425c87b9cdddf17f9ff06552d29e01aa
Samie <samrasolomon40@gmail.com>
2021-08-27 07:14:39 -0700

    Modified a file

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - to printout number
+ *
+ *Return: value 0
+ */
+int main(void)
+{
+int i = 0;
+while (i < 10)
+{
+printf("%d", i);
+i++;
+printf("\n")
+}
+return (0);
+}
