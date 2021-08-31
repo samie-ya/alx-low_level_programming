1aa07c55779b0f574d2dbd77369164c7f8a774b1
Samie <samrasolomon40@gmail.com>
2021-08-31 13:34:42 +0000

    Modified a file

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,10 +1,10 @@
-# include "main.h"
+#include "main.h"
 /**
 * main - to print alphabet in lower case
 *
 * Return: value 0
 */
-int main(void)
+void print_alphabet(void)
 {
 char print_alphabet;
 for (print_alphabet = 'a'; print_alphabet <= 'z'; print_alphabet++)
@@ -12,5 +12,5 @@ for (print_alphabet = 'a'; print_alphabet <= 'z'; print_alphabet++)
 _putchar(print_alphabet);
 }
 _putchar('\n');
-return (0);
+return;
 }
--- 0x02-functions_nested_loops/1-main.c
+++ 0x02-functions_nested_loops/1-main.c
@@ -5,7 +5,8 @@
  *
  * Return: Always 0.
  */
-void print_alphabet(void)
+int main(void)
 {
     print_alphabet();
+    return (0);
 }
