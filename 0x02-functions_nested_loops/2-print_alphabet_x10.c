1a2ef3a89b6caf016f54b34b35bfa5459e9fe2e2
Samie <samrasolomon40@gmail.com>
2021-08-31 13:21:11 +0000

    Added a file

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,16 +7,16 @@
 */
 int main(void)
 {
-char print_alphabet_x10;
 int i = 0;
-while (i <= 10)
+while (i < 10)
 {
-for ((print_alphabet_x10 = 'a'); (print_alphabet_x10 = 'z'); (print_alphabet_x10++)) 
+char print_alphabet_x10;
+for ((print_alphabet_x10 = 'a'); (print_alphabet_x10 <= 'z'); (print_alphabet_x10++)) 
 {
 _putchar(print_alphabet_x10);
 }
+_putchar('\n');
 i++;
 }
-_putchar('\n');
 return (0);
 }
