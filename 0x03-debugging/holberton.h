aa3bf36531585db1ae94e512a5e3550d526cb0cf
Samie <samrasolomon40@gmail.com>
2021-09-01 12:20:54 +0000

    Modified a file

--- 0x03-debugging/holberton.h
+++ 0x03-debugging/holberton.h
@@ -1,7 +1,9 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
 
+void positive_or_negative(int i);
 int convert_day(int month, int day);
 void print_remaining_days(int month, int day, int year);
+
 
 #endif
