2d6f7866bcfd4ed39eb8e2f1a7c225b8a392aec6
Samie <samrasolomon40@gmail.com>
2021-09-01 19:43:22 +0000

    Added a file

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+* main - print 50 fibonacci 
+*
+* Return: value 0
+*/
+int main(void)
+{
+int i;
+int num1 = 1;
+int num2 = 2;
+int num3 = num1 + num2;
+int num4 = 50;
+printf("%d, %d, ", num1, num2);
+for (i = 3; i <= num4; ++i)
+{
+printf("%d, ", num3);
+num1 = num2;
+num2 = num3;
+num3 = num1 + num2;
+}
+printf("\n");
+return (0);
+}
