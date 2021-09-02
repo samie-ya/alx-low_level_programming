5178b99f1cf3ae9ba08390d34150676f489976ea
Samie <samrasolomon40@gmail.com>
2021-09-02 18:16:19 +0000

    Modified a file

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -17,6 +17,11 @@ if (month == 2 && day == 60)
 printf("Day of the year: %d\n", day);
 printf("Remaining days: %d\n", 366 - day);
 }
+else
+{
+printf("Day of the year: %d\n", day);
+printf("Remaining days: %d\n", 366 - day);
+}
 }
 else
 {
