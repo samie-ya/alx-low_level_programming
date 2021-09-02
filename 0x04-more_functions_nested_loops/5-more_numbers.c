f8e56942a6bb4f884e7995951fe329ba1005a281
Samie <samrasolomon40@gmail.com>
2021-09-02 15:55:54 +0000

    Modified a file

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -13,14 +13,8 @@ for (a = 0; a < 10; a++)
 for (b = 0; b <= 14; b++)
 {
 if (b > 9)
-{
 _putchar((b / 10) + '0');
 _putchar((b % 10) + '0');
-}
-else
-{
-_putchar((b % 10) + '0');
-}
 }
 _putchar('\n');
 }
