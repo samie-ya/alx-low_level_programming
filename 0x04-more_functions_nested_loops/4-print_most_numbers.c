e4d657bd63a82ad7fa8be9c3f2ff2cc03f70bee7
Samie <samrasolomon40@gmail.com>
2021-09-02 12:48:25 +0000

    Modified a file

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -2,8 +2,6 @@
 /**
 * print_most_numbers- doesn't print 2 and 4
 *
-* @a: parameter used
-*
 * Return: nothing
 */
 void print_most_numbers(void)
@@ -11,9 +9,8 @@ void print_most_numbers(void)
 int a;
 for (a = '0'; a <= '9'; a++)
 {
-if ((a != '2') && (a != '4'))
+if (a != '2' && a != '4')
 _putchar(a);
 }
 _putchar('\n');
-return;
 }
--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -10,17 +10,9 @@
 */
 void more_numbers(void)
 {
-int a;
-int b = 0;
-while (b < 9)
-{
-for (a = '0'; a <= 14; a++)
-{
-_putchar(a + '0');
-}
-_putchar(b);
-b++;
-}
-_putchar('\n');
+int a = 0;
+while (a < 10)
+_putchar(a);
+a++;
 return;
 }
