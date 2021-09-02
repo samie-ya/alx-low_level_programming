b722b70a6e641765bacc6a53ea066ae99848b0ae
Samie <samrasolomon40@gmail.com>
2021-09-02 12:51:26 +0000

    Modified a file

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -4,23 +4,17 @@
 *
 * @n: parameter used
 *
-* @i: used as a parameter
-*
 * Return: nothing
 */
 void print_line(int n)
 {
 int i;
 if (n <= 0)
-{
 _putchar('\n');
-}
 if (n > 0)
 {
 for (i = 0; i < n; i++)
-{
 _putchar('_');
-}
 }
 _putchar('\n');
 return;
