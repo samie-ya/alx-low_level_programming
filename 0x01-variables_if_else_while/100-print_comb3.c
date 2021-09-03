cc2b744e2b77cac1c5c6767624acc4464f142d98
Samie <samrasolomon40@gmail.com>
2021-09-03 18:23:57 +0000

    Modified a file

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,12 +10,16 @@ int i;
 int j;
 for (i = 0; i < 9; i++)
 {
-for (j = i + 1; j < 10; j++)
+for (j = i + 1; j <= 9; j++)
+{
 {
 putchar((i % 10) + '0');
 putchar((j % 10) + '0');
-if ((i == 8) && (i == 9))
+}
+if (i == 8 && j == 9)
+{
 continue;
+}
 putchar(',');
 putchar(' ');
 }
