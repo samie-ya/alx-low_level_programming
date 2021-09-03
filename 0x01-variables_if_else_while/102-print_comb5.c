de4721a7895f6d35696dbc80c199d6c9a19a8cce
Samie <samrasolomon40@gmail.com>
2021-09-03 19:54:51 +0000

    Added a file

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+/**
+* main- prints 4 digits
+*
+* Return: value 0
+*/
+int main(void)
+{
+int i;
+int j;
+int l;
+int m;
+for (i = 0; i <= 9; i++)
+{
+for (j = 0; j <= 8; j++)
+{
+for (l = 0; l <= 9; l++)
+{
+for (m = 1; m <= 9; m++)
+{
+putchar((i % 10) + '0');
+putchar((j % 10) + '0');
+putchar(' ');
+putchar((l % 10) + '0');
+putchar((m % 10) + '0');
+if (i == 9 && j == 8 && l == 9 && m == 9)
+{
+continue;
+}
+putchar(',');
+putchar(' ');
+}
+}
+}
+putchar('\n');
+}
+return (0);
+}
