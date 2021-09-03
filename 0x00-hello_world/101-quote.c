a843c2d4f9625f88f44004a806f3fa9d41d5b56a
Samie <samrasolomon40@gmail.com>
2021-09-03 14:01:16 +0000

    Modified a file

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,5 @@
 #include <unistd.h>
+#include <stdio.h>
 /**
  * main - will be printing a statement and returning one value.
  *
@@ -6,10 +7,8 @@
  */
 int main(void)
 {
-if
 {
-write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 59);
-return (-1);
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 }
 return (0);
 }
