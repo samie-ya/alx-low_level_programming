29655de400ad733bf8954b64ccde467e3d9aa9b4
Samie <samrasolomon40@gmail.com>
2021-09-06 15:36:37 +0000

    Modified a file

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,23 +1,22 @@
 #include "main.h"
 /**
+* print_rev- prints in reverse
 *
+* @s: variable used
 *
-*
-*
-*
-*
-*
+* Return: value 0
 */
 void print_rev(char *s)
 {
 int i = 0;
+int j;
 while (s[i] != '\0')
 {
 i++;
 }
-for (i = 0; i = (i - 1); i--)
+for (j = i - 1; j >= 0; --j)
 {
-_putchar(s[i]);
+_putchar(s[j]);
 }
 _putchar('\n');
 }
