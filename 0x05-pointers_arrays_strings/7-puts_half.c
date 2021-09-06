538e1ce7e94bbb870575addeb83b7222229885d0
Samie <samrasolomon40@gmail.com>
2021-09-06 18:47:01 +0000

    Modified a file

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,22 +1,25 @@
 #include "main.h"
 /**
+* puts_half- prints half of string
 *
+* @str: parameter used
 *
-*
-*
-*
+* Return: value nothing
 *
 */
 void puts_half(char *str)
 {
 int i = 0;
+int n;
+int k;
 while (str[i] != '\0')
 {
 i++;
 }
-for (i = 0; i += 1; i++)
+n = i / 2;
+for (k = n; k <= i; k++)
 {
-_putchar(*str);
+_putchar(str[k]);
 }
 _putchar('\n');
 }
