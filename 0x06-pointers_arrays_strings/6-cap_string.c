84b8a3a9c37ddaea817ad23f06ebd0537fd907e3
Samie <samrasolomon40@gmail.com>
2021-09-09 15:47:39 +0000

    Modified a file

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,25 +1,21 @@
 #include "main.h"
 /**
+* cap_string- capitalizes the first letter
 *
+* @s: parameter used
 *
-*
-*
-*
-*
-*
-*
+* Return: value s
 */
 char *cap_string(char *s)
 {
 int i;
-char a[] = {32, 9, 10, ',', ';', '.', '!', '?', '"', '(', ')', '{', '}'};
-int j = 13;
-int k;
+char a[10] = {',', ';', '.', '!', '?', '"', '(', ')', '{', '}'};
+int j;
 for (i = 0; s[i] != '\0'; i++)
 {
-for (k = 0; k < j; k++)
+for (j = 0; j < 10; j++)
 {
-if (s[i] == a[k])
+if (s[i] == a[j])
 {
 i++;
 if (s[i] >= 'a' && s[i] <= 'z')
@@ -29,18 +25,14 @@ continue;
 }
 }
 }
-if (i == 0)
+if (s[i] == '\n' || s[i] == '\t' || s[i] == ' ')
 {
+i++;
 if (s[i] >= 'a' && s[i] <= 'z')
 {
 s[i] = s[i] - 32;
 continue;
-} 
 }
-else
-{
-if (s[i] >= 'A' && s[i] <= 'Z')
-s[i] = s[i] + 32;
 }
 }
 return (s);
