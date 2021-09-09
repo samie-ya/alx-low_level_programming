6b26d712fb91d841e283238b45c0c3ea4fb28696
Samie <samrasolomon40@gmail.com>
2021-09-09 16:50:33 +0000

    Modified a file

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,30 +1,18 @@
 #include "main.h"
 /**
-*
-*
-*
-*
-*
-*
-*
-*
-*/
+ *reverse_array - For printing an integer array in reverse order
+ *@a: pointer to the string
+ *@n: number of elements in the array
+ *
+ *no return since its void
+ */
 void reverse_array(int *a, int n)
 {
-int i = 0;
-int j;
-int k;
-while (a[i] != '\0')
-{
-i++;
-}
-j = 0;
-k = i < n;
-for (j = 0; j ; k++)
-{
-aux = a[k];
-a[k] = *p;
-*p = aux;
-p--;
-}
+        int i;
+        for (i = 0; i < (n / 2); i++)
+        {
+            int swap = a[n - 1 - i];
+            a[n - 1 - i] = a[i];
+            a[i] = swap;
+        }
 }
