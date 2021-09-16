8cbd9a5f39e97014f4c6d6c9875218bcac4a5195
Samie <samrasolomon40@gmail.com>
2021-09-16 09:53:32 +0000

    Modified a file

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -23,15 +23,17 @@ return _prime(n, 1);
 */
 int _prime (int n, int i)
 {
-if (n <= 1) 
+if (n % i == 0 && i > 1)
 {
 return (0);
 }
-if (n % i == 0)
+if (n <= 1)
 {
 return (0);
 }
 if ((n / i) < i)
+{
 return (1);
+}
 return (_prime(n, i + 1));
 }
