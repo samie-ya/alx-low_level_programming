444a3ad629c78db26a2fd8da5f120964fec5c265
Samie <samrasolomon40@gmail.com>
2021-09-20 20:40:32 +0000

    Modified a file

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -8,12 +8,13 @@
 */
 void print_number(int n)
 {
-unsigned int i = n;
-if (i < 0)
+unsigned int;
+if (n < 0)
 {
 _putchar('-');
-i = -i;
+n *= -1;
 }
+i = n;
 if ((i / 10) > 0)
 print_number(i / 10);
 _putchar ((i % 10) + '0');
