ac36a0611b15e5025f31df0e5b9bb8118bf6ef7e
Samie <samrasolomon40@gmail.com>
2021-09-27 23:55:59 +0000

    Addeda  file

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -9,11 +9,13 @@
 */
 void print_dog(struct dog *d)
 {
+if (d == 0)
+return;
 if (d->name == 0)
 printf("Name: (nil)\n");
 else
 printf("Name: %s\n", d->name);
-if (d->age == 0)
+if (d->age < 0)
 printf("Age: (nil)\n");
 else
 printf("Age: %f\n", d->age);
@@ -21,6 +23,4 @@ if (d->owner == 0)
 printf("Owner: (nil)\n");
 else
 printf("Owner: %s\n", d->owner);
-if (d == 0)
-return;
 }
