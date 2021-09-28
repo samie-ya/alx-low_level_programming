5a76bb71a440a37de66d069f355e54d6f6722cc2
Samie <samrasolomon40@gmail.com>
2021-09-28 07:07:50 +0000

    Modified a file

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,19 +1,19 @@
 #include "dog.h"
 #include <stdlib.h>
 /**
+* free_dog- to free the memory allocated by dog
 *
+* @d: pointer used
 *
-*
-*
-*
-*
-*
-*
+* Return: 0 - if d = 0
 */
 void free_dog(dog_t *d)
 {
 if (d == 0)
 {
-free(d);
+return;
 }
+free(d->owner);
+free(d->name);
+free(d);
 }
