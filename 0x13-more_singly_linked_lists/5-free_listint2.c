277e1aff884dd6331e27aba368d837b3ecfe6509
Samie <samrasolomon40@gmail.com>
2021-10-12 21:12:23 +0000

    Modified a file

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -13,11 +13,14 @@ void free_listint2(listint_t **head)
 {
 	listint_t *next;
 
+	if (*head == NULL)
+	{
+		return;
+	}
 	while (*head != NULL)
 	{
 		next = *head;
 		*head = (*head)->next;
 		free(next);
 	}
-	head = NULL;
 }
