f18129531610effdfdce28ef2954edadbe3e37e5
Samie <samrasolomon40@gmail.com>
2021-10-12 18:18:44 +0000

    Modified a file

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -2,25 +2,27 @@
 #include <stdlib.h>
 
 /**
-*
-*
-*
-*
-*
-*
-*
-*
-*/
+ * pop_listint- to delete the head of the node
+ *
+ * @head: pointer to pointer head
+ *
+ * Return: if list is not empty- the data of the head to be deleted
+ *         if list is  empty- return 0.
+ */
 
 int pop_listint(listint_t **head)
 {
+	listint_t *new;
 	int i;
 
-	while (*head != NULL)
-	{
-		i = (*head)->n;
-		*head = (*head)->next;
-		free(*head);
-		return (i);
-	}
+		while (*head != NULL)
+		{
+			new = *head;
+			i = (*head)->n;
+			*head = (*head)->next;
+			free(new);
+			return (i);
+		}
+
+	return (0);
 }
