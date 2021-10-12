0d99d8d046ed470f9eadc24fcce697f636c5e0ab
Samie <samrasolomon40@gmail.com>
2021-10-12 18:58:17 +0000

    Added and Modified files

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -19,5 +19,5 @@ void free_listint2(listint_t **head)
 		*head = (*head)->next;
 		free(next);
 	}
-	head = NULL;
+	free(*head);
 }
--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -2,52 +2,53 @@
 #include <stdlib.h>
 
 /**
-*
-*
-*
-*
-*
-*
-*
-*
-*
-*/
+ * insert_nodeint_at_index- inserts node at a point
+ *
+ * @head: pointer to pointer head
+ *
+ * @idx: the index of list where to add
+ *
+ * @n: the number to be associated with the new node
+ *
+ * Return: if head is not empty- return to address of new node
+ *         if head is empty- return  null
+ */
 
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	unsigned int i = 1;
 	listint_t *new;
 	listint_t *previous;
+	unsigned int i = 1;
 
-	new = malloc(sizeof(listint_t));
-	if (new == NULL)
+	previous = *head;
+	if (*head != NULL)
 	{
-		return (NULL);
-	}
-	new->n = n;
-	if (idx > 0)
-	{	
-		while (*head != NULL)
+		new = malloc(sizeof(listint_t));
+		if (new == NULL)
 		{
-			if (i == idx)
+			return (0);
+		}
+		new->n = n;
+		if (idx > 0)
+		{
+			while (previous != NULL)
 			{
-				previous = *head;
-				new->next = previous->next;
-				previous->next = new;
-				return (new);
+				if (i == idx)
+				{
+					new->next = previous->next;
+					previous->next = new;
+					return (new);
+				}
+				previous = previous->next;
+				i++;
 			}
-			if (i < idx)
-			{
-				return (new);
-			}
-			*head = (*head)->next;
-			i++;
+		}
+		else
+		{
+		new->next = *head;
+		*head = new;
+		return (new);
 		}
 	}
-	else
-	{
-	new->next = *head;
-	*head = new;
-	}
-	return (new);
+	return (NULL);
 }
