736e23c5c137164b48517c4269da569585a9cadf
Samie <samrasolomon40@gmail.com>
2021-10-14 16:42:33 +0000

    Added a file

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,17 +1,19 @@
 #include "main.h"
 
 /**
-*
-*
-*
-*
-*
-*/
+ * get_bit- to get the nth bit
+ *
+ * @n: number to be checked
+ *
+ * @index: the index to be checked starting from the right
+ *
+ * Return: the bit.
+ */
 
 int get_bit(unsigned long int n, unsigned int index)
 {
-int i;
+	int i;
 
-i = (n >> index) & 1;
-return (i);
+	i = (n >> index) & 1;
+	return (i);
 }
