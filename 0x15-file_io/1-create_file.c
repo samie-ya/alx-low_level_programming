40dbd245d2efec23525ce6529260e9ce75b351a5
Samie <samrasolomon40@gmail.com>
2021-10-19 02:54:49 +0000

    Modified a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -20,18 +20,21 @@ int create_file(const char *filename, char *text_content)
 	int fd, i, wr;
 
 	if (filename == NULL)
-		return (0);
+		return (-1);
 	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	if (fd == -1)
 		return (0);
-	i = 0;
-	while (text_content[i] != '\0')
+	if (text_content != NULL)
 	{
-		i++;
+		i = 0;
+		while (text_content[i] != '\0')
+		{
+			i++;
+		}
+		wr = write(fd, text_content, i);
+		if (wr == -1)
+			return (0);
 	}
-	wr = write(fd, text_content, i);
-	if (wr == -1)
-		return (0);
 	close(fd);
 	return (1);
 }
