fe3c0a6b7bd46d4c5cf373de6db2c39c32034017
Samie <samrasolomon40@gmail.com>
2021-10-19 01:04:32 +0000

    Added a file

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,40 @@
+#include "main.h"
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+/**
+ * append_text_to_file- to append a file
+ *
+ * @filename: pointer to the file
+ *
+ * @text_content: what is to be appended on a file
+ *
+ * Return: 1 on success
+ *         0 on failure
+ */
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fd, i, wr;
+
+	if (filename == NULL)
+		return (-1);
+	fd = open(filename, O_APPEND | O_WRONLY | O_EXCL);
+	if (fd == -1)
+		return (0);
+	i = 0;
+	while (text_content[i] != '\0')
+	{
+		i++;
+	}
+	if (text_content != NULL)
+	{
+		wr = write(fd, text_content, i);
+		if (wr == -1)
+			return (0);
+	}
+	close(fd);
+	return (1);
+}
