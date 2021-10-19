c464e915b2a6d706566d66835f527ffc9bc5eb7b
Samie <samrasolomon40@gmail.com>
2021-10-19 13:29:27 +0000

    Added a file

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,16 +1,30 @@
 #include "main.h"
+
 /**
-* print_number- prints number without hard coding
-*
-* Return: Nothing
-*/
+ * print_number- to print number without hard coding
+ *
+ * @n: the number to be printed
+ *
+ * Return: Nothing
+ */
+
 void print_number(int n)
 {
-if (n < 0)
-{
-n = (-1) * n;
-}
-if (n < 10)
-{
-_putchar((n) + '0');
+	int num;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		n *= -1;
+	}
+	num = n;
+	if (num / 10 > 0)
+	{
+		print_number(num / 10);
+		_putchar((num % 10) + '0');
+	}
+	else
+	{
+		_putchar(num + '0');
+	}
 }
