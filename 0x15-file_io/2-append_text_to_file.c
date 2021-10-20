ece2a5f28705675c2841229e0f49cdacb927aaef
Samie <samrasolomon40@gmail.com>
2021-10-20 03:18:40 +0000

    Modified a file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -24,13 +24,13 @@ int append_text_to_file(const char *filename, char *text_content)
 	fd = open(filename, O_APPEND | O_WRONLY);
 	if (fd == -1)
 		return (-1);
-	i = 0;
-	while (text_content[i] != '\0')
-	{
-		i++;
-	}
 	if (text_content != NULL)
 	{
+		i = 0;
+		while (text_content[i] != '\0')
+		{
+			i++;
+		}
 		wr = write(fd, text_content, i);
 		if (wr == -1)
 			return (-1);
