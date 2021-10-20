96a19cb597d23469d0955c193cdbb52f1584d18b
Samie <samrasolomon40@gmail.com>
2021-10-20 16:38:59 +0000

    Modified a file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -18,17 +18,15 @@
 
 int main(int ac, char **av)
 {
-	int fd, rd, fd1, wr, m;
-	char s[];
+	int fd, rd, fd1, wr;
+	char s[1024];
 
 	if (ac != 3)
 	{
-		dprintf(2, "Usage: cp file_from file_to");
+		dprintf(2, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
 	fd = open(av[1], O_RDONLY);
-	if (m == 1024)
-	{
 	rd = read(fd, s, m);
 	if (fd == -1 || rd == -1)
 	{
@@ -41,7 +39,6 @@ int main(int ac, char **av)
 	{
 		dprintf(2, "Error: Can't write to %s\n", av[2]);
 		exit(99);
-	}
 	}
 	if (close(fd) < 0)
 	{
