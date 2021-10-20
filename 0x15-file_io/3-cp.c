2fed83dfedcabe960f176ee3f5e85ae01e5d5b8b
Samie <samrasolomon40@gmail.com>
2021-10-20 16:16:15 +0000

    Modified a file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -27,20 +27,15 @@ int main(int ac, char **av)
 		exit(97);
 	}
 	fd = open(av[1], O_RDWR);
-	i = 0;
-	while (s[i] != '\0')
-	{
-		i++;
-	}
-	rd = read(fd, s, i);
-	if (fd == -1 && rd == -1)
+	rd = read(fd, s, 1024);
+	if (fd == -1 || rd == -1)
 	{
 		dprintf(2, "Error: Can't read from file %s\n", av[1]);
 		exit(98);
 	}
 	fd1 = open(av[2], O_CREAT | O_RDWR | O_TRUNC, 0664);
-	wr = write(fd1, s, rd);
-	if (fd1 == -1 && wr == -1)
+	wr = write(fd1, s, 1024);
+	if (fd1 == -1 || wr == -1)
 	{
 		dprintf(2, "Error: Can't write to %s\n", av[2]);
 		exit(99);
