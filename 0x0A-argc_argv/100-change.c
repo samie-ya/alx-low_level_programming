ba949d5dd65df48632bfd2810b5a4afa773624a2
Samie <samrasolomon40@gmail.com>
2021-10-21 10:08:27 +0000

    Modified a file

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,50 +1,55 @@
 #include "main.h"
 #include <stdio.h>
+#include <stdlib.h>
 /**
-*
-*
-*
-*
-*
-*
-*
-*/
-int main(int argc, char *argv[])
+ * main - to make change
+ *
+ * @ac: number of command lines
+ *
+ * @av: argument of command line
+ *
+ * Return: Always 0.
+ */
+int main(int ac, char *av[])
 {
-int a = atoi(argv[1]);
-int div;
-if (argc == 2)
-{
-if (a >= 25 && a % 25 == 0)
-{
-div = a / 25;
-}
-if (a >= 10 && a % 10 == 0)
-{
-div = a / 10;
-}
-if (a >= 5 && a % 5 == 0)
-{
-div = a / 5;
-}
-if (a >= 2 && a % 2 == 0)
-{
-div = a / 2;
-}
-if (a >= 1 && a % 1 == 0)
-{
-div = a / 1;
-}
-if (a < 0)
-{
-printf("0\n");
-}
-printf("%d\n", div);
-}
-else
+int coin, i = atoi(av[1]);
+if (ac == 1)
 {
 printf("Error\n");
 return (1);
 }
+if (i < 0)
+printf("0\n");
+coin = 0;
+while (i != 0)
+{
+coin++;
+if (i - 25 >= 0)
+{
+i = i - 25;
+continue;
+}
+if (i - 10 >= 0)
+{
+i = i - 10;
+continue;
+}
+if (i - 5 >= 0)
+{
+i = i - 5;
+continue;
+}
+if (i - 2 >= 0)
+{
+i = i - 2;
+continue;
+}
+if (i - 1 >= 0)
+{
+i = i - 1;
+continue;
+}
+}
+printf("%d\n", coin);
 return (0);
 }
