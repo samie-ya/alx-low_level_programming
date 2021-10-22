38674b89141b9c4b2c11965e5f7536bf6e7f2eec
Samie <samrasolomon40@gmail.com>
2021-10-22 09:39:40 +0000

    Modified a file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -18,6 +18,7 @@ int main(int ac, char **av)
 {
 	int fd, rd, fd1, wr;
 	char s[3000];
+
 	if (ac != 3)
 	{
 		dprintf(2, "Usage: cp file_from file_to\n");
@@ -30,17 +31,14 @@ int main(int ac, char **av)
 		exit(98);
 	}
 	rd = read(fd, s, 3000);
-	if (rd > 0)
+	fd1 = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	wr = write(fd1, s, rd);
+	if (fd1 == -1 || wr == -1)
 	{
-		fd1 = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-		wr = write(fd1, s, rd);
-		if (fd1 == -1 || wr == -1)
-		{
-			dprintf(2, "Error: Can't write to %s\n", av[2]);
-			exit(99);
-		}
+		dprintf(2, "Error: Can't write to %s\n", av[2]);
+		exit(99);
 	}
-	else
+	if (rd == -1)
 	{
 		dprintf(2, "Error: Can't read from file %s\n", av[1]);
 		exit(98);
@@ -55,5 +53,7 @@ int main(int ac, char **av)
 		dprintf(2, "Error: Can't close fd %d\n", fd1);
 		exit(100);
 	}
+	close(fd);
+	close(fd1);
 	return (0);
 }
