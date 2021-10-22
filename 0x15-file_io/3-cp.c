fdfe1b28606966852d535c50b3e7db70e14b568e
Samie <samrasolomon40@gmail.com>
2021-10-22 13:06:03 +0000

    Modified a file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -17,7 +17,7 @@
 int main(int ac, char **av)
 {
 	int fd, rd, fd1, wr;
-	char s[3000];
+	char *s;
 
 	if (ac != 3)
 	{
@@ -30,18 +30,24 @@ int main(int ac, char **av)
 		dprintf(1, "Error: Can't read from file %s\n", av[1]);
 		exit(98);
 	}
-	rd = read(fd, s, 3000);
-	fd1 = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-	wr = write(fd1, s, rd);
-	if (fd1 == -1 || wr == -1)
+	s = malloc(sizeof(char) * 1024);
+	if (s == NULL)
+		return (0);
+	rd = read(fd, s, 1024);
+	while (rd > 0)
 	{
-		dprintf(2, "Error: Can't write to %s\n", av[2]);
-		exit(99);
-	}
-	if (rd == -1)
-	{
-		dprintf(2, "Error: Can't read from file %s\n", av[1]);
-		exit(98);
+		if (rd == -1)
+		{
+			dprintf(2, "Error: Can't read from file %s\n", av[1]);
+			exit(98);
+		}
+		fd1 = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+		wr = write(fd1, s, rd);
+		if (fd1 == -1 || wr == -1)
+		{
+			dprintf(2, "Error: Can't write to %s\n", av[2]);
+			exit(99);
+		}
 	}
 	if (close(fd) == -1)
 	{
