ae5a2d2fc32ba64867e48ebaf52ad3c67d8b73cd
Samie <samrasolomon40@gmail.com>
2021-10-22 09:35:37 +0000

    Modified a file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -16,44 +16,46 @@
  */
 int main(int ac, char **av)
 {
-int fd, rd, fd1, wr;
-char s[3000];
-if (ac != 3)
-{
-dprintf(2, "Usage: cp file_from file_to\n");
-exit(97);
-}
-fd = open(av[1], O_RDONLY);
-if (fd == -1)
-{
-dprintf(1, "Error: Can't read from file %s\n", av[1]);
-exit(98);
-}
-rd = read(fd, s, 3000);
-if (rd > 0)
-{
-fd1 = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-wr = write(fd1, s, rd);
-if (fd1 == -1 || wr == -1)
-{
-dprintf(2, "Error: Can't write to %s\n", av[2]);
-exit(99);
-}
-}
-else
-{
-dprintf(2, "Error: Can't read from file %s\n", av[1]);
-exit(98);
-}
-if (close(fd) == -1)
-{
-dprintf(2, "Error: Can't close fd %d\n", fd);
-exit(100);
-}
-if (close(fd1) == -1)
-{
-dprintf(2, "Error: Can't close fd %d\n", fd1);
-exit(100);
-}
-return (0);
+	int fd, rd, fd1, wr;
+	char s[1024];
+	if (ac != 3)
+	{
+		dprintf(2, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+	fd = open(av[1], O_RDONLY);
+	if (fd == -1)
+	{
+		dprintf(1, "Error: Can't read from file %s\n", av[1]);
+		exit(98);
+	}
+	rd = read(fd, s, 1024);
+	if (rd >= 0)
+	{
+		fd1 = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+		wr = write(fd1, s, rd);
+		if (fd1 == -1 || wr == -1)
+		{
+			dprintf(2, "Error: Can't write to %s\n", av[2]);
+			exit(99);
+		}
+	}
+	else
+	{
+		dprintf(2, "Error: Can't read from file %s\n", av[1]);
+		exit(98);
+	}
+	if (close(fd) == -1)
+	{
+		dprintf(2, "Error: Can't close fd %d\n", fd);
+		exit(100);
+	}
+	if (close(fd1) == -1)
+	{
+		dprintf(2, "Error: Can't close fd %d\n", fd1);
+		exit(100);
+	}
+	close(fd);
+	close(fd1);
+	return (0);
 }
