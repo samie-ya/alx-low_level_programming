85886931698173c9ea8908a39d6775d264af8f78
Samie <samrasolomon40@gmail.com>
2021-10-22 13:15:23 +0000

    Modified a file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -24,30 +24,27 @@ int main(int ac, char **av)
 		dprintf(2, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-	fd = open(av[1], O_RDONLY);
-	if (fd == -1)
-	{
-		dprintf(1, "Error: Can't read from file %s\n", av[1]);
-		exit(98);
-	}
 	s = malloc(sizeof(char) * 1024);
 	if (s == NULL)
-		return (0);
+		exit(99);
+	fd = open(av[1], O_RDONLY);
 	rd = read(fd, s, 1024);
+	fd1 = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	while (rd > 0)
 	{
-		if (rd == -1)
+		if (rd == -1 || fd == -1)
 		{
 			dprintf(2, "Error: Can't read from file %s\n", av[1]);
 			exit(98);
 		}
-		fd1 = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 		wr = write(fd1, s, rd);
 		if (fd1 == -1 || wr == -1)
 		{
 			dprintf(2, "Error: Can't write to %s\n", av[2]);
 			exit(99);
 		}
+		rd = read(fd, s, 1024);
+		fd1 = open(av[2], O_WRONLY | O_APPEND);
 	}
 	if (close(fd) == -1)
 	{
@@ -59,6 +56,7 @@ int main(int ac, char **av)
 		dprintf(2, "Error: Can't close fd %d\n", fd1);
 		exit(100);
 	}
+	free(s);
 	close(fd);
 	close(fd1);
 	return (0);
