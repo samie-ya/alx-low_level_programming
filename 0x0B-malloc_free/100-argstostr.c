709719723f4f4176cceccc324776841c601303aa
Samie <samrasolomon40@gmail.com>
2021-10-23 18:03:03 +0000

    Modified a file

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,32 +1,51 @@
 #include "main.h"
 #include <stdlib.h>
 #include <stdio.h>
+
 /**
-*
-*
-*
-*
-*
-*
-*
-*
-*/
+ * argstostr - to concatenate a command line arguments to a string
+ *
+ * @ac: number of command line arguments passed
+ *
+ * @av: command argumnets
+ *
+ * Return: the string
+ */
+
 char *argstostr(int ac, char **av)
 {
-char *dest;
-if (ac == 0 || av == 0)
-{
-return (0);
-}
-dest = malloc(sizeof(char *) * ac);
-if (dest == 0)
-{
-return (0);
-}
-while(ac--)
-{
-dest = *av;
-printf("%s\n", dest++);
-}
-return (dest);
+	int i, j, len, k = 0;
+	char *s, *dest;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+	for (i = 0; i < ac; i++)
+	{
+		s = av[i];
+		j = 1;
+		while (s[j])
+		{
+			j++;
+			len++;
+		}
+	}
+	dest = malloc(sizeof(char) * (len + 1));
+	if (dest == NULL)
+	{
+		return (NULL);
+		free(dest);
+	}
+	for (i = 0; i < ac; i++)
+	{
+		s = av[i];
+		for (j = 0; s[j] != '\0'; j++)
+		{
+			dest[k] = s[j];
+			k++;
+		}
+		dest[k] = '\n';
+		k++;
+	}
+	dest[len] = '\0';
+	return (dest);
 }
