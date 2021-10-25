e754a8f21f7c6c0e6e6d62208e6fd47d229ab227
Samie <samrasolomon40@gmail.com>
2021-10-25 10:39:47 +0000

    Modified a file

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,55 +1,50 @@
 #include "main.h"
 #include <stdlib.h>
+
 /**
-* *string_nconcat- concatanates n bytes
-*
-* @s1: the string to contain the concatanates
-*
-* @s2: the string that contains n string
-*
-* @n: the number of string to concactanate
-*
-* Return: if function fails - 0
-*         if it works - dest
-*/
+ * string_nconcat - to concatenate the first n bytes to string
+ *
+ * @s1: string to be concatenated
+ *
+ * @s2: the string to concatenate
+ *
+ * @n: number of bytes to be concatenated
+ *
+ * Return: the concatenated string
+ */
+
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-unsigned int i = 0;
-unsigned int j = 0;
-unsigned int k = 0;
-unsigned int len = 0;
-char *dest;
-if (s1 == 0)
-s1 = "";
-if (s2 == 0)
-s2 = "";
-while (s1[i] != '\0')
-i++;
-while (s2[j] != '\0')
-j++;
-if (n >= j)
-len = i + j + 1;
-else
-len = i + n + 1;
-dest = malloc(len);
-if (dest == 0)
-{
-return (0);
-free(dest);
-}
-if (k < len)
-{
-for (i = 0; s1[i] != '\0'; i++)
-{
-dest[k] = s1[i];
-k++;
-}
-for (j = 0; s2[j] != '\0' && j < n; j++)
-{
-dest[k] = s2[j];
-k++;
-}
-}
-dest[k] = '\0';
-return (dest);
+	char *dest;
+	unsigned int i, j;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	i = 0;
+	while (s1[i] != '\0')
+	{
+		i++;
+	}
+	j = 0;
+	while (s2[j] != '\0')
+	{
+		j++;
+	}
+	dest = malloc(sizeof(char) * (i + j + 1));
+	if (dest == NULL)
+		return (NULL);
+	i = 0;
+	while (s1[i] != '\0')
+	{
+		dest[i] = s1[i];
+		i++;
+	}
+	for (j = 0; j < n; j++)
+	{
+		dest[i + j] = s2[j];
+	}
+	dest[i + j] = '\0';
+	return (dest);
 }
