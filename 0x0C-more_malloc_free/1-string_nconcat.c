17f8ae510107225faf199a8503c8aa7a9a6ec397
Samie <samrasolomon40@gmail.com>
2021-10-25 13:43:14 +0000

    Modified a file

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -41,7 +41,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{
 		dest[i] = s1[i];
 	}
-	for (j = 0; s2[j]&& j < n; j++)
+	for (j = 0; s2[j] && j < n; j++)
 	{
 		dest[i + j] = s2[j];
 	}
