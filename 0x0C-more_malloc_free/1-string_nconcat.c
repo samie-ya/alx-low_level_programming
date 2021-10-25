509189d9a06ab631df203a0e750c5abab84b2c06
Samie <samrasolomon40@gmail.com>
2021-10-25 11:09:56 +0000

    Modified a file

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -15,42 +15,37 @@
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-unsigned int i = 0;
-unsigned int j = 0;
-unsigned int k = 0;
-unsigned int len = 0;
-char *dest;
-if (s1 == 0)
-s1 = "";
-if (s2 == 0)
-s2 = "";
-while (s1[i] != '\0')
-i++;
-while (s2[j] != '\0')
-j++;
-if (n >= j)
-len = i + j + 1;
-else
-len = i + n + 1;
-dest = malloc(len);
-if (dest == 0)
-{
-return (0);
-free(dest);
-}
-if (k < len)
-{
-for (i = 0; s1[i] != '\0'; i++)
-{
-dest[k] = s1[i];
-k++;
-}
-for (j = 0; s2[j] != '\0' && j < n; j++)
-{
-dest[k] = s2[j];
-k++;
-}
-}
-dest[k] = '\0';
-return (dest);
+	unsigned int i, j, len;
+	char *dest;
+
+	if (s1 == 0)
+		s1 = "";
+	if (s2 == 0)
+	s2 = "";
+	i = 0;
+	while (s1[i] != '\0')
+		i++;
+	j = 0;
+	while (s2[j] != '\0')
+		j++;
+	if (n < j)
+		len = i + n;
+	else
+		len = i + j;
+	dest = malloc(sizeof(char) * (len + 1));
+	if (dest == NULL)
+	{
+		return (0);
+		free(dest);
+	}
+	for (i = 0; s1[i] != '\0'; i++)
+	{
+		dest[i] = s1[i];
+	}
+	for (j = 0; s2[j] != '\0'; j++)
+	{
+		dest[i + j] = s2[j];
+	}
+	dest[i + j] = '\0';
+	return (dest);
 }
