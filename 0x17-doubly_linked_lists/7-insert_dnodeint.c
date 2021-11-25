fe7e3b5fbd8ceef8cf8fd59c56bda7b0e336dc7f
Samie <samrasolomon40@gmail.com>
2021-11-25 15:09:25 +0000

    Modified a file

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -26,29 +26,24 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	if (new == NULL)
 		return (0);
 	new->n = n;
-	if (h == NULL)
-		return (NULL);
 	if (idx == 0)
-		add_dnodeint(h, n);
-	else if(idx == len)
-		add_dnodeint_end(h, n);
-	else
+		return (add_dnodeint(h, n));
+	else if (idx == len)
+		return (add_dnodeint_end(h, n));
+	while (head != NULL)
 	{
-		while (head != NULL)
+		if (idx == i)
 		{
-			if (idx == i)
-			{
-				current = head;
-				new->next = current;
-				previous = current->prev;
-				new->prev = previous;
-				previous->next = new;
-				current->prev = new;
-				return (new);
-			}
-			i++;
-			head = head->next;
+			current = head;
+			new->next = current;
+			previous = current->prev;
+			new->prev = previous;
+			previous->next = new;
+			current->prev = new;
+			return (new);
 		}
+		i++;
+		head = head->next;
 	}
 	return (NULL);
 }
