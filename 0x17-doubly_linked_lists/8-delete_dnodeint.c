fc54f5f150357743d72af6fd9d426a67b5ba28cc
Samie <samrasolomon40@gmail.com>
2021-11-26 10:34:58 +0000

    Modified a file

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -26,8 +26,8 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 			next->prev = NULL;
 			free(*head);
 			*head = next;
+			return (1);
 		}
-		return (1);
 	}
 	else if (index >= len)
 	{
