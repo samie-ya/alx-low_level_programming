f3ba5e20dd06b244407ea8f8df35756540c3775a
Samie <samrasolomon40@gmail.com>
2021-12-17 16:24:55 +0000

    Modified a file

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -12,6 +12,7 @@ void hash_table_print(const hash_table_t *ht)
 {
 	unsigned long int i;
 	hash_node_t *node;
+	int j = 0;
 
 	if (ht == NULL)
 		return;
@@ -21,7 +22,10 @@ void hash_table_print(const hash_table_t *ht)
 		node = ht->array[i];
 		while (node != NULL)
 		{
+			if (j == 1)
+				printf(", ");
 			printf("'%s': '%s'", node->key, node->value);
+			j = 1;
 			node = node->next;
 		}
 	}
