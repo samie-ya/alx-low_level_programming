183e7c04bc41e198a3d38b23228063a0d7270f2d
Samie <samrasolomon40@gmail.com>
2021-12-19 18:28:29 +0000

    Added a file

---
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+* find_listint_loop- finds a loop in a list
+*
+* @head: pointer to the head of the list
+*
+* Return: the address of the node
+*/
+
+listint_t *find_listint_loop(listint_t *head)
+{
+	listint_t *slow = head, *fast = head;
+
+	while (slow != NULL && fast != NULL && fast->next != NULL)
+	{
+		slow = slow->next;
+		fast = fast->next->next;
+		if (fast == slow)
+		{
+			return (slow);
+		}
+	}
+	return (NULL);
+}
--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,6 +1,9 @@
-#ifndef LIST_H
-#define LIST_H
+#ifndef LISTS_H
+#define LISTS_H
+
 #include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 
 /**
@@ -32,4 +35,7 @@ int sum_listint(listint_t *head);
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
 int delete_nodeint_at_index(listint_t **head, unsigned int index);
 listint_t *reverse_listint(listint_t **head);
+listint_t *find_listint_loop(listint_t *head);
+size_t print_listint_safe(const listint_t *head);
+
 #endif
