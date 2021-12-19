5a87dbc53e4a854890330064fc9407067ebef46b
Samie <samrasolomon40@gmail.com>
2021-12-19 14:50:35 +0000

    Modified a file

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+void before_main(void) __attribute__((constructor));
+
+/**
+* before_main- function that is executed before main
+*
+* Return: Nothing
+*/
+
+void before_main(void)
+{
+printf("You're beat! and yet, you must allow,\n"
+"I bore my house upon my back!\n");
+}
