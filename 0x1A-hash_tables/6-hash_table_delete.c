5de6b6edb116de6e6363fcd23a66ca0ab44d98ab
Samie <samrasolomon40@gmail.com>
2021-12-22 14:38:25 +0000

    Modified a file

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -11,36 +11,27 @@
 void hash_table_delete(hash_table_t *ht)
 {
 	unsigned long int i;
-	hash_node_t *to_remove = NULL, *node = NULL, *next = NULL;
+	hash_node_t *to_remove, *node, *next;
 
-	if (ht != NULL && ht->array != NULL)
+	for (i = 0; i < ht->size; i++)
 	{
-		for (i = 0; i < ht->size; i++)
+		to_remove = ht->array[i];
+		if (to_remove != NULL)
 		{
-			to_remove = ht->array[i];
-			if (to_remove != NULL && to_remove->next == NULL)
+			node = to_remove->next;
+			while (node != NULL)
 			{
-				if (to_remove->next != NULL && to_remove->next->next == NULL)
-				{
-					node = to_remove->next;
-					while (node->next != NULL)
-					{
-						next = node->next;
-						free(node->key);
-						free(node->value);
-						free(node);
-						node = next;
-					}
-					free(node->key);
-					free(node->value);
-					free(node);
-				}
-				free(to_remove->key);
-				free(to_remove->value);
-				free(to_remove);
+				next = node->next;
+				free(node->key);
+				free(node->value);
+				free(node);
+				node = next;
 			}
+			free(to_remove->key);
+			free(to_remove->value);
+			free(to_remove);
 		}
-		free(ht->array);
-		free(ht);
 	}
+	free(ht->array);
+	free(ht);
 }
